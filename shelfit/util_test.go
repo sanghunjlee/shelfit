7f5a5b6c34a525fd7bdc0fe09aac2515d2104ba5
kyo <sanghunjlee@gmail.com>
2023-03-12 14:40:27 -0700

    Added test for ljust function

--- shelfit/util_test.go
+++ shelfit/util_test.go
@@ -17,3 +17,16 @@ func BenchmarkParseRangeInt(b *testing.B) {
 		parseRangeInt(input)
 	}
 }
+
+func TestLjust(t *testing.T) {
+	test := "hello world"
+	if t1 := ljust(test, 4); t1 != "hell" {
+		t.Errorf(`ljust(%s, 4) != "hell"`, test)
+	}
+	if t2 := ljust(test, 20); len(t2) != 20 {
+		t.Errorf(`len(ljust(%s, 20)) != 20`, test)
+	}
+	if t3 := ljust(test, 0); t3 != "" {
+		t.Errorf(`ljust(%s, 0) != ""`, test)
+	}
+}
